f0c6307826b14a3850b9985e17185cb9c22c76b7
Daniel Dadds <danieldadds95@gmail.com>
2023-05-17 23:19:56 +0100

    styling and search feature added

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const feelsLike = document.getElementById("feels-like");
 const currentHumid = document.getElementById("humidity");
 const currentWind = document.getElementById("wind");
 
+const searchBar = document.getElementById("search");
+const searchSubmit = document.getElementById("submit");
+
+let weatherCurrent;
+function createWeather(location, img, weather, temp, feel, humid, wind) {
+  return {
+    location: location,
+    img: img,
+    weather: weather,
+    temp: temp,
+    feel: feel,
+    humid,
+    humid,
+    wind: wind,
+  };
+}
+
 function searchWeather(location) {
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=92506e3ad17146d28bd121434231705&q=${location}`,
@@ -23,7 +40,13 @@ function searchWeather(location) {
       feelsLike.innerText = response.current.feelslike_c;
       currentHumid.innerText = response.current.humidity;
       currentWind.innerText = response.current.wind_mph;
+
+      //   console.log(weatherCurrent);
     });
 }
 
+searchSubmit.addEventListener("click", function () {
+  let tempLocation = searchBar.value;
+  searchWeather(tempLocation);
+});
 searchWeather("Belfast");
